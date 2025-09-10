26667fafce34a0eac33306bd3efd7741dc0809ec
yj960932-source <yj960932@gmail.com>
2025-09-10 08:31:09 +0000

    내용 변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 hello younjun")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")."
 )
